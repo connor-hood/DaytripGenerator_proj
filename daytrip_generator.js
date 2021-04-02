7c11d61b595674514e971a805ffbd67eba4444c2
Connor Hood <connor_hood@yahoo.com>
2021-04-02 14:53:01 -0500

    added prompts for user input, started function to test if user input matches values in array, will generate random result if input is null or does not match

--- daytrip_generator.js
+++ daytrip_generator.js
@@ -5,17 +5,41 @@ let restaurantSelection = ["Zios", "Runza", "Canes", "Block 16"];
 let transportationSelection = ["Ollie the Trollie", "Metro", "Uber", "Horseback"];
 let entertainmentSelection = ["Stand up at Funny Bone", "Random Craft workshop", "Farmer's Market", "Play at Playhouse"];
 
-function getRandomValue(selectionArray){
-    let randomValue = Math.floor(Math.random() * selectionArray.length);
-    console.log(selectionArray[randomValue]);
-    return selectionArray[randomValue];
+let userInputDest = prompt("What part of town would you like to go to?");
+let userInputRest = prompt("Where would you like to have lunch catered by?");
+let userInputTran = prompt("What is your preferred mode of transportation?");
+let userInputEnt = prompt("What activity would you like to do?");
+
+function checkForRandom(input, selectionArray, finalArray){
+    let randomValue = Math.floor(Math.random() * selectionArray.length); 
+    for (let i = 0; i < selectionArray.length; i++) {
+        if (input == null || input != array[i]){
+            finalArray.push(randomValue)
+        }
+        
+    }
 }
 
+
+
+
+/* function getRandomValue(selectionArray){
+    
+    return selectionArray[randomValue];
+} */
+/* 
 let destination = getRandomValue(destinationSelection);
 let restaurant = getRandomValue(restaurantSelection);
 let transportation = getRandomValue(transportationSelection);
-let entertainment = getRandomValue(entertainmentSelection);
+let entertainment = getRandomValue(entertainmentSelection); */
 
 let finalSelectionArray = [];
 
-console.log("Ok, you trip has been planned! You will be going to " + destination + ". Lunch will be provided by: " + restaurant + ". We will be getting to " + entertainment + " via " + transportation + ".");
+function addToFinal(option, finalArray){
+    finalArray.push(option);
+}
+addToFinal(destination, finalSelectionArray);
+addToFinal(restaurant, finalSelectionArray);
+addToFinal(transportation, finalSelectionArray);
+addToFinal(entertainment, finalSelectionArray);
+console.log("Ok, you trip has been planned! You will be going to " + finalSelectionArray[0] + ". Lunch will be provided by: " + finalSelectionArray[1] + ". We will be getting to " + finalSelectionArray[2] + " via " + finalSelectionArray[3] + ".");
