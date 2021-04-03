e3efd093723b1d64d4718668fbcacb73df4a5afb
Connor Hood <connor_hood@yahoo.com>
2021-04-03 10:27:44 -0500

    built confirm random function, will test out. need to build empty array for final commit

--- daytrip_generator.js
+++ daytrip_generator.js
@@ -5,20 +5,22 @@ let restaurantSelection = ["Zios", "Runza", "Canes", "Block 16"];
 let transportationSelection = ["Ollie the Trollie", "Metro", "Uber", "Horseback Drawn Carridge"];
 let entertainmentSelection = ["Stand up at Funny Bone", "Random Craft workshop", "Farmer's Market", "Play at Playhouse"];
 
-let userInputDest = prompt("What part of town would you like to go to?");
-let userInputRest = prompt("Where would you like to have lunch catered by?");
-let userInputTran = prompt("What is your preferred mode of transportation?", "Ollie The trollie, Metro, Uber, Horseback Drawn Carridge");
-let userInputEnt = prompt("What activity would you like to do?", "Stand up at Funny Bone, Random Craft Workshop, Farmer's Market, Play at Playhouse");
-
-let finalSelectionArray = [];
-
-function checkForRandom(input, selectionArray, finalArray){
-    let randomValue = Math.floor(Math.random() * selectionArray.length); 
-    for (let i = 0; i < selectionArray.length; i++) {
-        if (input === selectionArray[i]){
+function getRandomValue(selectionArray){
+    let randomValue = Math.floor(Math.random() * selectionArray.length);
+    return selectionArray[randomValue];
 }
 
-console.log(checkForRandom(userInputDest, destinationSelection, finalSelectionArray));
-console.log(checkForRandom(userInputRest, restaurantSelection, finalSelectionArray));
-console.log(checkForRandom(userInputTran, transportationSelection, finalSelectionArray));
-console.log(checkForRandom(userInputEnt, entertainmentSelection, finalSelectionArray));
+getRandomValue(destinationSelection); 
+
+function confirmRandom (randomValue, finalArray){
+    let verify = confirm("Please confirm " + randomValue);
+        if (verify == true){
+            finalArray.push(randomValue);
+        } 
+}
+let destination = getRandomValue(destinationSelection);
+let restaurant = getRandomValue(restaurantSelection);
+let transportation = getRandomValue(transportationSelection);
+let entertainment = getRandomValue(entertainmentSelection);
+
+console.log("Ok, you trip has been planned! You will be going to " + destination + ". Lunch will be provided by: " + restaurant + ". We will be getting to " + entertainment + " via " + transportation + ".");
