4665c54116ad559087e0095f8d3eab36d778156f
Connor Hood <connor_hood@yahoo.com>
2021-04-03 09:25:17 -0500

    started almost from scratch, started on random function, will  begin working on prompts

--- daytrip_generator.js
+++ daytrip_generator.js
@@ -2,44 +2,23 @@
 
 let destinationSelection = ["Benson", "Aksarben", "Village Pointe", "Blackstone", "Old Market"];
 let restaurantSelection = ["Zios", "Runza", "Canes", "Block 16"];
-let transportationSelection = ["Ollie the Trollie", "Metro", "Uber", "Horseback"];
+let transportationSelection = ["Ollie the Trollie", "Metro", "Uber", "Horseback Drawn Carridge"];
 let entertainmentSelection = ["Stand up at Funny Bone", "Random Craft workshop", "Farmer's Market", "Play at Playhouse"];
 
 let userInputDest = prompt("What part of town would you like to go to?");
 let userInputRest = prompt("Where would you like to have lunch catered by?");
-let userInputTran = prompt("What is your preferred mode of transportation?");
-let userInputEnt = prompt("What activity would you like to do?");
+let userInputTran = prompt("What is your preferred mode of transportation?", "Ollie The trollie, Metro, Uber, Horseback Drawn Carridge");
+let userInputEnt = prompt("What activity would you like to do?", "Stand up at Funny Bone, Random Craft Workshop, Farmer's Market, Play at Playhouse");
+
+let finalSelectionArray = [];
 
 function checkForRandom(input, selectionArray, finalArray){
     let randomValue = Math.floor(Math.random() * selectionArray.length); 
     for (let i = 0; i < selectionArray.length; i++) {
-        if (input == null || input != array[i]){
-            finalArray.push(randomValue)
-        }
-        
-    }
+        if (input === selectionArray[i]){
 }
 
-
-
-
-/* function getRandomValue(selectionArray){
-    
-    return selectionArray[randomValue];
-} */
-/* 
-let destination = getRandomValue(destinationSelection);
-let restaurant = getRandomValue(restaurantSelection);
-let transportation = getRandomValue(transportationSelection);
-let entertainment = getRandomValue(entertainmentSelection); */
-
-let finalSelectionArray = [];
-
-function addToFinal(option, finalArray){
-    finalArray.push(option);
-}
-addToFinal(destination, finalSelectionArray);
-addToFinal(restaurant, finalSelectionArray);
-addToFinal(transportation, finalSelectionArray);
-addToFinal(entertainment, finalSelectionArray);
-console.log("Ok, you trip has been planned! You will be going to " + finalSelectionArray[0] + ". Lunch will be provided by: " + finalSelectionArray[1] + ". We will be getting to " + finalSelectionArray[2] + " via " + finalSelectionArray[3] + ".");
+console.log(checkForRandom(userInputDest, destinationSelection, finalSelectionArray));
+console.log(checkForRandom(userInputRest, restaurantSelection, finalSelectionArray));
+console.log(checkForRandom(userInputTran, transportationSelection, finalSelectionArray));
+console.log(checkForRandom(userInputEnt, entertainmentSelection, finalSelectionArray));
