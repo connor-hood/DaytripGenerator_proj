9d1004f6aa3de120e0f357bb13c932127800fc9d
Connor Hood <connor_hood@yahoo.com>
2021-04-04 09:45:55 -0500

    built confirm function, will test with destination first

--- daytrip_generator.js
+++ daytrip_generator.js
@@ -5,6 +5,15 @@ let restaurantSelection = ["Zios", "Runza", "Canes", "Block 16"];
 let transportationSelection = ["Ollie the Trollie", "Metro", "Uber", "Horseback Drawn Carridge"];
 let entertainmentSelection = ["Stand up at Funny Bone", "Random Craft workshop", "Farmer's Market", "Play at Playhouse"];
 
+function confirmValue(selection, finalArray){
+    let confirm = confirm("Please verify your selection : " + selection);
+    if (confirm === true) {
+        finalArray.push(confirm);
+    }
+}
+
+
+
 function getRandomValue(selectionArray){
     let randomValue = Math.floor(Math.random() * selectionArray.length);
     return selectionArray[randomValue];
@@ -14,20 +23,8 @@ getRandomValue(destinationSelection);
 
 let confirmedArrray = [];
 
-function confirmRandom (randomValue, randomValueArray, finalArray){
-    let verify = confirm("Please confirm " + randomValue);
-    let nextRandom;
-    let nextRandomVerify;
-    for (let i = 0; i < randomValueArray.length; i++) {
-    }
-        if (verify == true){
-            finalArray.push(randomValue);
-        } else{
-            confirm("Please confirm ")
-        }
-}
 let destination = getRandomValue(destinationSelection);
-confirmRandom(destination, confirmedArrray);
+
 let restaurant = getRandomValue(restaurantSelection);
 let transportation = getRandomValue(transportationSelection);
 let entertainment = getRandomValue(entertainmentSelection);
